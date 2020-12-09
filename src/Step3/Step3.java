d2f4484fa1069569e4d1f3237a86e563147d64f4
bibi6666667 <bibi6666667@kakao.com>
2020-12-09 18:12:37 +0900

    [Step3] 구현해야 할 메서드 틀 제작 및 몇몇 메서드 구현
    - 구현해야 할 모든 메서드 틀을 미리 만들어 놓음.
    - trimInput(), checkInput(), guideInput() 구현 -
    
    TODO : 로직 연결 잘 되는지 테스트 및 나머지 메서드들 구현!

--- src/Step3/Step3.java
+++ src/Step3/Step3.java
@@ -27,22 +27,22 @@ public class Step3 { // 루빅스 큐브 구현하기
         char[][] initRight = new char[3][3];
         char[][] initDown = new char[3][3];
         char[][] initBack = new char[3][3];
-        initFront[0] = new char[]{'G', 'G', 'G'}; // init Front = Green
+        initFront[0] = new char[]{'G', 'G', 'G'}; // initFront : Green
         initFront[1] = new char[]{'G', 'G', 'G'};
         initFront[2] = new char[]{'G', 'G', 'G'};
-        initUp[0] = new char[]{'W', 'W', 'W'}; // init Up = White
+        initUp[0] = new char[]{'W', 'W', 'W'}; // initUp : White
         initUp[1] = new char[]{'W', 'W', 'W'};
         initUp[2] = new char[]{'W', 'W', 'W'};
-        initLeft[0] = new char[]{'O', 'O', 'O'}; // init Left = Orange
+        initLeft[0] = new char[]{'O', 'O', 'O'}; // initLeft : Orange
         initLeft[1] = new char[]{'O', 'O', 'O'};
         initLeft[2] = new char[]{'O', 'O', 'O'};
-        initRight[0] = new char[]{'R', 'R', 'R'}; // init Right = Red
+        initRight[0] = new char[]{'R', 'R', 'R'}; // initRight : Red
         initRight[1] = new char[]{'R', 'R', 'R'};
         initRight[2] = new char[]{'R', 'R', 'R'};
-        initDown[0] = new char[]{'Y', 'Y', 'Y'}; // init Down = Yellow
+        initDown[0] = new char[]{'Y', 'Y', 'Y'}; // initDown : Yellow
         initDown[1] = new char[]{'Y', 'Y', 'Y'};
         initDown[2] = new char[]{'Y', 'Y', 'Y'};
-        initBack[0] = new char[]{'B', 'B', 'B'}; // init Back = Blue
+        initBack[0] = new char[]{'B', 'B', 'B'}; // initBack : Blue
         initBack[1] = new char[]{'B', 'B', 'B'};
         initBack[2] = new char[]{'B', 'B', 'B'};
         cubeFront = initFront;
@@ -57,6 +57,8 @@ public class Step3 { // 루빅스 큐브 구현하기
         printCube();
         String input = start();
         System.out.println("input : " + input);
+        ArrayList<String> inputList = trimInput(input);
+        checkInput(inputList);
     }
 
     public void printCube() {
@@ -94,5 +96,126 @@ public class Step3 { // 루빅스 큐브 구현하기
         return input;
     }
 
-}
+    public ArrayList<String> trimInput(String input) { // 입력값 다듬기
+        String[] inputArr = input.split("");
+        int inputArrLength = inputArr.length;
+        ArrayList<String> inputList = new ArrayList<>();
+        for (int i = 0; i < inputArrLength; i++) {
+            String inputElement = inputArr[i];
+            if (inputElement.equals("'")) {
+                inputArr[i - 1] = inputArr[i - 1] + "'";
+            }
+        }
+        for (int i = 0; i < inputArrLength ; i++) {
+            String inputElement = inputArr[i];
+            if (!inputElement.equals("'")) {// 요소가 '이 아닐 때 inputList에 추가
+                inputList.add(inputElement);
+            }
+        }
+        return inputList;
+    }
+
+    public void checkInput(ArrayList<String> inputList) {
+        int inputListSize = inputList.size();
+        for (int i = 0; i < inputListSize; i++) {
+            String anInput = inputList.get(i);
+            if (anInput.equals("U") || anInput.equals("U'") || anInput.equals("F") || anInput.equals("F'")
+                    || anInput.equals("L") || anInput.equals("L'") || anInput.equals("R") || anInput.equals("R'")
+                    || anInput.equals("Q")) { // U U' F F' L L' R R' Q 일 때만.
+                guideInput(anInput);
+            } else {
+                System.out.println("❗ 지정되지 않은 명령어가 포함되어 있습니다. 다시 입력해 주세요.");
+                String reInput = start();
+                ArrayList<String> reInputList = trimInput(reInput);
+                checkInput(reInputList);
+            }
+            if (i == (inputListSize - 1)) {
+                ready();
+            }
+        }
+       // ready(); - 이렇게 고쳐도 동일?
+    }
+
+    public void guideInput(String anInput) {
+        char[][] tempFront = new char[3][3];
+        char[][] tempUp = new char[3][3];
+        char[][] tempLeft = new char[3][3];
+        char[][] tempRight = new char[3][3];
+        char[][] tempDown = new char[3][3];
+        char[][] tempBack = new char[3][3];
+        switch(anInput) {
+            case "U" :
+                whenU(tempUp, tempLeft, tempFront, tempRight, tempBack);
+                break;
+            case "U'" :
+                whenUDot(tempUp, tempLeft, tempFront, tempRight, tempBack);
+                break;
+            case "F" :
+                whenF(tempFront, tempUp, tempLeft, tempRight, tempDown);
+                break;
+            case "F'" :
+                whenFDot(tempFront, tempUp, tempLeft, tempRight, tempDown);
+                break;
+            case "L" :
+                whenL(tempLeft, tempUp, tempFront, tempDown, tempBack);
+                break;
+            case "L'" :
+                whenLDot(tempLeft, tempUp, tempFront, tempDown, tempBack);
+                break;
+            case "R" :
+                whenR(tempRight, tempUp, tempFront, tempDown, tempBack);
+                break;
+            case "R'" :
+                whenRDot(tempRight, tempUp, tempFront, tempDown, tempBack);
+                break;
+            case "Q" :
+                sc.close();
+                System.out.println("종료메시지");
+                System.exit(0);
+                break;
+        }
+        printCube();
+        System.out.println(); // 줄바꿈
+    }
+
+    // TODO :  guideInput()과 함께 whenU(), whenU'() .. 및 copyCube() 구현하기.
+
+    public void copyCube() { // 매개변수 여러개 받아서 한쪽에서 한쪽으로 복사하기.
+
+    }
+
+    // public void pasteCube() {} // 되도록 copyCube()만으로 해결하기?
+
+    public void whenU(char[][] tempUp, char[][] tempLeft, char[][] tempFront, char[][] tempRight, char[][] tempBack) {
+
+    }
+
+    public void whenUDot(char[][] tempUp, char[][] tempLeft, char[][] tempFront, char[][] tempRight, char[][] tempBack) {
+
+    }
+
+    public void whenF(char[][] tempFront, char[][] tempUp, char[][] tempLeft, char[][] tempRight, char[][] tempDown) {
+
+    }
+
+    public void whenFDot(char[][] tempFront, char[][] tempUp, char[][] tempLeft, char[][] tempRight, char[][] tempDown) {
+
+    }
+
+    public void whenL(char[][] tempLeft, char[][] tempUp, char[][] tempFront, char[][] tempDown, char[][] tempBack) {
+
+    }
+
+    public void whenLDot(char[][] tempLeft, char[][] tempUp, char[][] tempFront, char[][] tempDown, char[][] tempBack) {
+
+    }
+
+    public void whenR(char[][] tempRight, char[][] tempUp, char[][] tempFront, char[][] tempDown, char[][] tempBack) {
+
+    }
+
+    public void whenRDot(char[][] tempRight, char[][] tempUp, char[][] tempFront, char[][] tempCubeDown, char[][] tempBack) {
+
+    }
+} // class
 
