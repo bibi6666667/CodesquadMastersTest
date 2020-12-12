7a3e94a726f7e20cd06aad35e22fc1b3ba6758d3
bibi6666667 <bibi6666667@kakao.com>
2020-12-12 23:06:26 +0900

    [Step1][Step2][Step3] 리팩토링 및 README.md 필기 업데이트
    - step1, step2, step3 코드들을 리팩토링하고 그 내용을 README.md필기에 반영.
    - README.md 필기를 현재 step1, step2, step3 상태에 맞게 업데이트
    - 각 코드에 대한 설명 수정 및 추가

--- src/Step1/Step1.java
+++ src/Step1/Step1.java
@@ -2,23 +2,22 @@ package Step1;
 
 import java.util.Scanner;
 
-public class Step1Controller {
-    Scanner sc = new Scanner(System.in); // 맨 마지막에 닫기
+public class Step1 {
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Step1Controller controller = new Step1Controller();
-        controller.starter();
+        Step1 step1 = new Step1();
+        step1.starter();
     }
 
     public void starter() {
         System.out.println("< step1. 단어 밀어내기 >");
         String word = inputWord();
         String checkedWord = wordCheck(word);
-        String num = inputNum();
-        String checkedNum = numCheck(num);
+        int num = inputNum();
+        int checkedNum = numCheck(num);
         String LR = inputLR();
         String checkedLR = LRCheck(LR);
-        System.out.println(checkedWord + "," + checkedNum + "," + checkedLR);
         trimInput(checkedWord, checkedNum, checkedLR);
     }
 
@@ -39,18 +38,16 @@ public class Step1Controller {
         }
     }
 
-    public String inputNum() {
+    public int inputNum() {
         System.out.println("숫자를 입력해 주세요. 입력한 숫자만큼 밀어냅니다.");
         System.out.println("-100이상, 100미만의 정수로 입력해 주세요.");
-        String num = sc.nextLine();
+        int num = sc.nextInt();
         return num;
     }
 
-    public String numCheck(String num) {
-        int intNum = Integer.parseInt(num);
-        if (intNum >= -100 && intNum < 100) {
-            String strNum = Integer.toString(intNum);
-            return strNum;
+    public int numCheck(int num) {
+        if (num >= -100 && num < 100) {
+            return num;
         } else {
             System.out.println("Error : -100 이상 100 미만의 정수를 입력해 주세요");
             num = inputNum();
@@ -60,8 +57,7 @@ public class Step1Controller {
     }
 
     public String inputLR() {
-        System.out.println("L(l) 또는 R(r)을 입력해 주세요. ");
-        System.out.println("L 입력시 왼쪽으로, R 입력시 오른쪽으로 밀어냅니다.");
+        System.out.println("L(l) 또는 R(r)을 입력해 주세요.\nL 입력시 왼쪽으로, R 입력시 오른쪽으로 밀어냅니다.");
         String LR = sc.nextLine();
         return LR;
     }
@@ -77,17 +73,17 @@ public class Step1Controller {
         }
     }
 
-    public void trimInput(String word, String num, String LR) { // 각각 함수로 만들고 리턴값 준 다음 pushWord에서 호출해 쓰기
-        System.out.println("checkedWord:" + word + " checkedNum:" + num + " checkedLR:" + LR);
-        if (LR.equals("l")) LR = "L"; // LR) 소문자 입력은 대문자로 변환
+    public void trimInput(String word, int num, String LR) {
+        // LR) 소문자 입력은 대문자로 변환
+        if (LR.equals("l")) LR = "L";
         if (LR.equals("r")) LR = "R";
-        int intNum = Integer.parseInt(num); // num) 음수는 LR반대로 & 양수로 하고, 양수는 word.length로 나눈 나머지로 치환
-        if (intNum < 0) { // num이 음수이면
+        // num) 음수는 양수로 & LR반대로 만들고, 만들어진 양수는 word.length로 나눈 나머지로 치환
+        if (num < 0) {
             if (LR.equals("L")) LR = "R"; // 왼->오, 오->왼
             else if (LR.equals("R")) LR = "L";
-            intNum *= -1; // 양수로 만들기
+            num *= -1; // 양수로 만들기
         }
-        int pushNum = intNum % word.length();
+        int pushNum = num % word.length(); // pushNum = 실제 밀어내는 숫자
         // word) 그대로 반환
         pushWord(word, pushNum, LR);
     }
@@ -97,11 +93,13 @@ public class Step1Controller {
         String wordLeft = null;
         String wordRight = null;
         int wordLength = word.length();
-        if (LR.equals("L")) {
+        if (LR.equals("L")) { // 왼쪽으로 밀어낼 때
+            // : 앞에서부터 num번째까지 잘라내어(wordRight) 나머지 글자(wordLeft)의 뒤에 붙임.
             wordLeft = word.substring(num, wordLength);
             wordRight = word.substring(0, num);
         }
-        if (LR.equals("R")) {
+        if (LR.equals("R")) {// 오른쪽으로 밀어낼 떄
+            // : 뒤에서부터 num번째까지 잘라내어(wordLeft) 나머지 글자(wordRight)의 앞에 붙임.
             wordLeft = word.substring(wordLength - num, wordLength);
             wordRight = word.substring(0, wordLength - num);
         }
